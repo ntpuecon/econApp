7e49a9b598d3408d0408e26ba1d189967e3724a6
tpemartin <tpemartin@gmail.com>
2022-03-20 16:48:50 +0800

    update

--- R/statisticContentFrame.R
+++ R/statisticContentFrame.R
@@ -5,7 +5,11 @@ tag_statisticContentFrame <-function(time="去年同月"){
     tags$div(class = "bcInMonth-light",
       tags$div(class = "bcInMonth-light-lightFrame",
         tags$div(class = "bcInMonth-light-lightFrame-lightImg")),
-      tags$div(class = "bcInMonth-light-ligtbulb")))
+      tags$div(class = "bcInMonth-light-ligtbulb",
+        tags$img(
+          src="https://ntpuecon.github.io/econApp/assets/img/lightbulb.svg",
+          height="20px", width="14px"
+        ))))
 }
 statisticContentFrame_dependency <- function(){
   htmltools::htmlDependency(
