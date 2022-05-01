e44c242a9b30e78f84701b966dfb59329060f7b9
tpemartin <tpemartin@gmail.com>
2022-05-01 11:29:49 +0800

    update

---
+++ R/dependencies.R
@@ -0,0 +1,20 @@
+ebookcard_dependency <-econData_dependency <- pageHeader_dependency<-maskBtn_dependency <- footer2_dependency <- page_dependency <- function(){
+  htmltools::htmlDependency(
+    name="econApp",
+    version="1.0.0",
+    src=c(file=system.file("/assets",  package = "econApp")),
+    style=c("css/page-master.css","css/statisticCard-master.css", "css/econData.css", "css/ebookcard.css"),
+    all_files = T
+  )}
+
+dep_ios <- function(){
+  econWeb::htmlDependency2(
+    name="ios-app",
+    version="0.0.1",
+    meta = list(
+      `apple-mobile-web-app-capable`="yes",
+      `apple-mobile-web-app-status-bar-style`="black"
+    )
+  )
+}
+
