8f3681d4fc511e4ec8aee81c475716e2b20bcd0e
Jewel4869 <71367839+Jewel4869@users.noreply.github.com>
2022-05-28 20:37:36 +0800

    update

--- R/clubinfo.R
+++ R/clubinfo.R
@@ -8,14 +8,14 @@ tag_clubinfo <-function(){
            tags$div(class = "info-icon"),
            tags$div(class = "info-content"))
 }
-clubinfo_dependency <- function(){
-  htmltools::htmlDependency(
-    name="econApp",
-    version="1.0.0",
-    src=c(file=system.file("/assets/css",  package = "econApp")),
-    style="clubinfo.css",
-    all_files = F
-  )}
+# clubinfo_dependency <- function(){
+#   htmltools::htmlDependency(
+#     name="econApp",
+#     version="1.0.0",
+#     src=c(file=system.file("/assets/css",  package = "econApp")),
+#     style="clubinfo.css",
+#     all_files = F
+#   )}
 ui_clubinfo <- function(dependency=NULL){
   tagList(tag_clubinfo(), clubinfo_dependency(), dependency)
 }
