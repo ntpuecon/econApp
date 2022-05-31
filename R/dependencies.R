e66bdf4d48c50c2df3f2bce4761e0794a0a56052
linttttt <91177710+linttttt@users.noreply.github.com>
2022-05-31 10:22:30 +0800

    update

--- R/dependencies.R
+++ R/dependencies.R
@@ -1,17 +1,11 @@
-<<<<<<< HEAD
-interna_dependency <- coursemap_dependency <-classes_dependency <- lectureCard_dependency <- equipmentCard_dependency <- pillButton_dependency <- calendar_dependency <- clubinfo_dependency <- ebookcard_dependency <-econData_dependency <- pageHeader_dependency<-maskBtn_dependency <- footer2_dependency <- page_dependency <- function(){
-=======
-classMap_footer_dependency <- clubinfo_dependency <- coursemap_dependency <- lectureCard_dependency <- simplePillButton_dependency <- equipmentCard_dependency <- calendar_dependency <- pillButton_dependency<- requireclass_dependency <-ebookcard_dependency <-econData_dependency <-pageHeader_dependency<-maskBtn_dependency <- footer2_dependency <- page_dependency <- function(){
->>>>>>> 467ad9d99cf41cd31eb7b659413df1b323e36c6e
+interna_dependency <- classes_dependency <- classMap_footer_dependency <- clubinfo_dependency <- coursemap_dependency <- lectureCard_dependency <- simplePillButton_dependency <- equipmentCard_dependency <- calendar_dependency <- pillButton_dependency<- requireclass_dependency <-ebookcard_dependency <-econData_dependency <-pageHeader_dependency<-maskBtn_dependency <- footer2_dependency <- page_dependency <- function(){
+
   htmltools::htmlDependency(
     name="econApp",
     version="1.0.0",
     src=c(file=system.file("/assets",  package = "econApp")),
-<<<<<<< HEAD
-    style=c("css/page-master.css","css/statisticCard-master.css", "css/econData.css", "css/ebookcard.css", "css/clubinfo.css", "css/calendar.css", "css/pillButton.css", "css/equipmentCard.css", "css/lectureCard.css", "css/classes.css", "css/coursemap.css","css/interna.css"),
-=======
-    style=c("css/page-master.css","css/statisticCard-master.css", "css/econData.css", "css/ebookcard.css","css/requireclass.css", "css/pillButton.css", "css/calendar.css", "css/equipmentCard.css","css/simplePillButton.css", "css/lectureCard.css", "css/coursemap.css","css/clubinfo-master.css", "css/classMap_footer.css"),
->>>>>>> 467ad9d99cf41cd31eb7b659413df1b323e36c6e
+    style=c("css/page-master.css","css/statisticCard-master.css", "css/econData.css", "css/ebookcard.css","css/requireclass.css", "css/pillButton.css", "css/calendar.css", "css/equipmentCard.css","css/simplePillButton.css", "css/lectureCard.css", "css/coursemap.css","css/clubinfo-master.css", "css/classMap_footer.css", "css/classes.css","css/interna.css"),
+
     all_files = T
   )}
 
