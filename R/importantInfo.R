29668826b7bd49f28f4f4cb2438bb659a80471d8
linttttt <91177710+linttttt@users.noreply.github.com>
2022-06-14 00:39:49 +0800

    update

---
+++ R/importantInfo.R
@@ -0,0 +1,25 @@
+fig_importantInfo <- function() {
+  fig <- econWeb::Fig()
+  fig$export("inst/assets/css/importantInfo")
+}
+tag_importantInfo <-function(){
+  tags$div(class = "importantInfo",
+           tags$div(class = "importantInfo-sorts",
+                    tags$div(class = "importantInfo-sorts-alumni",
+                             tags$div(class = "importantInfo-sorts-alumni-word", "系友會資訊")),
+                    tags$div(class = "importantInfo-sorts-internship",
+                             tags$div(class = "importantInfo-sorts-internship-word", "實習資訊"))),
+           tags$div(class = "importantInfo-info"))
+}
+importantInfo_dependency <- function(){
+  htmltools::htmlDependency(
+    name="econApp",
+    version="1.0.0",
+    src=c(file=system.file("/assets/css", package = "econApp")),
+    style="importantInfo.css",
+    all_files = F
+  )}
+importantInfo <- function(dependency=NULL){
+  tagList(tag_importantInfo(), importantInfo_dependency(), dependency)
+}
+#importantInfo() |> econWeb::browseTag2()
