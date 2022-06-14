f4b468f9a9ae0c909fefe05f6cb86267d4ccf692
linttttt <91177710+linttttt@users.noreply.github.com>
2022-06-14 16:41:06 +0800

    update

--- R/importantInfo.R
+++ R/importantInfo.R
@@ -1,13 +1,33 @@
+#' Title
+#'
+#' @param kind a character. "系友會資訊" or "實習資訊"
+#' @param dependency
+#'
+#' @return
+#' @export
+#'
+#' @examples none.
+importantInfo <- function(kind="系友會資訊", dependency=NULL){
+  tagList(tag_importantInfo(kind), importantInfo_dependency(), dependency)
+}
+
+
+# helper ------------------------------------------------------------------
+
+
+
 fig_importantInfo <- function() {
   fig <- econWeb::Fig()
   fig$export("inst/assets/css/importantInfo")
 }
-tag_importantInfo <-function(){
+tag_importantInfo <-function(kind){
   tags$div(class = "importantInfo",
            tags$div(class = "importantInfo-sorts",
                     tags$div(class = "importantInfo-sorts-alumni",
+                             style = htmltools::css(`background-color`= alumniColor(kind)),
                              tags$div(class = "importantInfo-sorts-alumni-word", "系友會資訊")),
                     tags$div(class = "importantInfo-sorts-internship",
+                             style = htmltools::css(`background-color`= internshipColor(kind)),
                              tags$div(class = "importantInfo-sorts-internship-word", "實習資訊"))),
            tags$div(class = "importantInfo-info"))
 }
@@ -19,7 +39,20 @@ tag_importantInfo <-function(){
 #     style="importantInfo.css",
 #     all_files = F
 #   )}
-importantInfo <- function(dependency=NULL){
-  tagList(tag_importantInfo(), importantInfo_dependency(), dependency)
+alumniColor <- function(click){
+  switch(
+    click,
+    "系友會資訊"={
+      "rgba(242, 153, 74, 0.2)"
+    },
+    "實習資訊"="#FFFFFF")
+}
+internshipColor <- function(click){
+  switch(
+    click,
+    "系友會資訊"={
+      "#FFFFFF"
+    },
+    "實習資訊"="rgba(242, 153, 74, 0.2)")
 }
 #importantInfo() |> econWeb::browseTag2()
